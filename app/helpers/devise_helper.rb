1e278da3308e0d5d9820d868cdd854eb803b5da8
SANTIAGO PEREZ <perezperret@gmail.com>
2016-12-18 14:18:05 -0400

    style login and errors

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,8 +1,17 @@
 module DeviseHelper
   def devise_error_messages!
-    return "" unless devise_error_messages?
+    flash_alerts = []
 
-    render partial: 'shared/errors', locals: { messages: resource.errors.full_messages }
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+    end
+
+    return "" if resource.errors.empty? && flash_alerts.empty?
+    messages = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
+
+    render partial: 'shared/errors', locals: { messages: messages }
   end
 
   def devise_error_messages?
