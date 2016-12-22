82ef2173fe7ca8db23ece918efb7fdf3cafbd064
SANTIAGO PEREZ <perezperret@gmail.com>
2016-12-22 19:24:16 -0400

    migrate to reflect new schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20161217020230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "chat_rooms", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_chat_rooms_on_user_id", using: :btree
-  end
-
   create_table "conversations", force: :cascade do |t|
     t.integer  "user_1_id"
     t.integer  "user_2_id"
@@ -61,7 +53,6 @@ ActiveRecord::Schema.define(version: 20161217020230) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "chat_rooms", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
 end
