c826d3680e20f35f20abd138af30a12d1b0f87fe
Santiago Perez Perret <perezperret@gmail.com>
2018-09-23 15:42:58 -0400

    Add link to root in Navbar

--- app/javascript/views/Navbar.js
+++ app/javascript/views/Navbar.js
@@ -16,8 +16,10 @@ class Navbar extends React.Component {
       <div className="container py-3">
         <div className="d-flex justify-content-between align-items-center">
           <div className="d-flex align-items-center">
-            <img src={this.props.logo} style={{ width: '60px' }} />
-            <h5 className="ml-1 mb-0 d-none d-md-inline-block">{this.props.title}</h5>
+            <a href="/">
+              <img src={this.props.logo} style={{ width: '60px' }} />
+              <h5 className="ml-1 mb-0 d-none d-md-inline-block">{this.props.title}</h5>
+            </a>
           </div>
 
           {this.state.currentUser
