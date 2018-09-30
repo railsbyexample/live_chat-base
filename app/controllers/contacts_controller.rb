fe47e5377a9e84e42d90b8da248b491b5bf2729b
Santiago Perez Perret <perezperret@gmail.com>
2018-09-30 17:29:55 -0400

    Add generic react view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,29 +1,45 @@
 class ContactsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :set_contact, only: [:update, :destroy]
 
   # GET /contacts
-  # GET /contacts.json
   def index
-    @contacts = current_user.contacts.confirmed
+    contacts = current_user.contacts.confirmed
+
+    render '/shared/react_view',
+           locals: {
+             component_name: 'contacts/Index',
+             prop_name: 'contacts',
+             prop_partial: 'contacts/contacts',
+             prop_partial_locals: { contacts: contacts, included: { contact: %i[last_message] } }
+           }
   end
 
   # GET /contacts/1
-  # GET /contacts/1.json
   def show
+    contact = Contact.find(params[:id])
+    render '/shared/react_view',
+           locals: {
+             component_name: 'contacts/Show',
+             prop_name: 'contact',
+             prop_partial: 'contacts/contact',
+             prop_partial_locals: { contact: contact, included: %i[messages] }
+           }
   end
 
   # GET /contacts/new
   def new
-    @unconfirmed_contacts = current_user.contacts.unconfirmed
-  end
-
-  # GET /contacts/1/edit
-  def edit
+    unconfirmed_contacts = current_user.contacts.unconfirmed
+    render '/shared/react_view',
+           locals: {
+             component_name: 'contacts/New',
+             prop_name: 'unconfirmed_contacts',
+             prop_partial: 'contacts/contacts',
+             prop_partial_locals: { contacts: unconfirmed_contacts }
+           }
   end
 
   # POST /contacts
-  # POST /contacts.json
   def create
     @contact = Contact.new
     receiver_email = params[:contact][:email]
@@ -62,7 +78,6 @@ class ContactsController < ApplicationController
   end
 
   # PATCH/PUT /contacts/1
-  # PATCH/PUT /contacts/1.json
   def update
     unless @contact.receiver == current_user
       redirect_to contacts_url, alert: "You can't confirm your own invitation"
@@ -78,7 +93,6 @@ class ContactsController < ApplicationController
   end
 
   # DELETE /contacts/1
-  # DELETE /contacts/1.json
   def destroy
     unless @contact.sender == current_user || @contact.receiver == current_user
       redirect_to contacts_url, alert: "You can't access this record"
@@ -93,13 +107,12 @@ class ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def contact_params
-      params.require(:contact).permit(:sender_id, :receiver_id, :confirmed_at)
-    end
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:sender_id, :receiver_id, :confirmed_at)
+  end
 end
