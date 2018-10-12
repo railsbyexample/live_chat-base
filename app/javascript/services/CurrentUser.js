e6402df4eb0e2bdd16d77d64e517c874dfa8da69
Santiago Perez Perret <perezperret@gmail.com>
2018-10-12 15:26:58 -0400

    Handle CurrentUser not present in navbar

--- app/javascript/services/CurrentUser.js
+++ app/javascript/services/CurrentUser.js
@@ -1,4 +1,11 @@
-const getCurrentUser = () => document.querySelector('meta[name=current_user]').getAttribute('content')
+const getCurrentUser = () => {
+  const userIsSignedIn = document.querySelector('meta[name=user_signed_in]').getAttribute('content')
+
+  if (userIsSignedIn == "true") {
+    return document.querySelector('meta[name=current_user]').getAttribute('content')
+  }
+}
+
 const CurrentUser = { getCurrentUser }
 
 export default CurrentUser
